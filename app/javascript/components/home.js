2e2f8f6a6c55ecdec08b6e4bff31d3be031982d6
Romain Campech <romain.campech@city.ac.uk>
2018-01-27 19:31:10 +0000

    Set up home page

---
+++ app/javascript/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../styles/home'
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>INCLUB</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/javascript/packs/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Home from '../components/home'
+
+render(
+  <Home />,
+  document.getElementById('home-hook')
+)
